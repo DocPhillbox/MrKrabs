9ab56e63ecf6284a7e5694423593a1431788c9c7
Andrey Bastien <71886221+DocPhillbox@users.noreply.github.com>
2022-05-04 14:52:44 +0200

    add url arguments

--- script.py
+++ script.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', help="The url where dowload media file", type=str)
+args = vars(parser.parse_args())
 
 def get_html_from_url(url: str):
     req = requests.get(url)
     return req.text
 
-htmldata = get_html_from_url("https://www.geeksforgeeks.org/")
+htmldata = get_html_from_url(args['url'] or "https://www.geeksforgeeks.org/")
 soup = BeautifulSoup(htmldata, 'html.parser')
 for image in soup.find_all('img'):
     image_src = image.get('src')
-    image_name = Path(image_src).name
-    image_data = requests.get(image_src).content
-    open('out/' + image_name, 'wb').write(image_data)
+    if image_src:
+        print('download file:', image_src)
+        image_name = Path(image_src).name
+        image_data = requests.get(image_src).content
+        open('out/' + image_name, 'wb').write(image_data)
